30f1f1b302b00f3cac72e8913fce8181f645ae42
Thomas Charles Elswick <finlaymirkwood@gmail.com>
2015-03-02 13:09:19 -0700

    Now it's even better. I know, I'm amazing.

--- src/helloWorld/HelloWorld.java
+++ src/helloWorld/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	{
 		System.out.println("Hello World!!");
 		System.out.println("This is clrearly better than before.");
+		System.out.println("And now it is the bestest.");
 	}
 
 }
