6d2811b874aec2fab83dee3fc40b29745410e8ad
Thomas Charles Elswick <finlaymirkwood@gmail.com>
2015-03-13 14:30:31 -0600

    Not a lot of code was added for this update, but the changes are pretty substatial. I added in the shapes that will be used for collision detection and have them all rotating the way they are supposed to. This was one of the main concerns I had which prompted this test so I am pleased.

--- src/com/burningbushgames/test/GameWindow.java
+++ src/com/burningbushgames/test/GameWindow.java
@@ -7,16 +7,21 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Circle;
 import org.newdawn.slick.geom.Polygon;
 import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.geom.Shape;
+import org.newdawn.slick.geom.Transform;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class GameWindow extends BasicGameState {
 	
 	private Rectangle background;
-	private Shape rectangle;
+	private Shape attachedBox;
+	private Shape floatingBox;
+	private Shape levelHolder;
+	private Shape ground;
 	private Image level;
 	private float scale;
 
@@ -24,13 +29,24 @@ public class GameWindow extends BasicGameState {
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException 
 	{
 		background = new Rectangle(0, 0, gc.getWidth(), gc.getHeight());
-		rectangle = new Polygon(new float[] {288,427, 253,431, 253,326, 290,362, 290,427, 392,366, 434,238, 359,237, 292,238, 293,154, 258,152, 361,238,});
-		rectangle.setCenterX(gc.getWidth()*.5F);
-		rectangle.setCenterY(gc.getHeight()*.5F);
+		
 		level = new Image("res/Circular-Level.gif");
 		scale = (gc.getWidth()/2F)/level.getWidth();
 		level = level.getScaledCopy(scale);
 		System.out.println(scale);
+		levelHolder = new Polygon(new float[] {0,0, level.getWidth(),0, level.getWidth(),level.getHeight(), 0,level.getHeight()});
+		levelHolder.setCenterX(gc.getWidth()/2);
+		levelHolder.setCenterY(gc.getHeight()/2);
+		
+		attachedBox = new Polygon(new float[] {250*scale,358*scale, 291*scale,358*scale, 290*scale,428*scale, 250*scale,431*scale,});
+		attachedBox.setX(levelHolder.getX() + attachedBox.getX());
+		attachedBox.setY(levelHolder.getY() + attachedBox.getY());
+		
+		floatingBox = new Polygon(new float[] {290*scale,149*scale, 361*scale,149*scale, 361*scale,240*scale, 290*scale,240*scale});
+		floatingBox.setX(levelHolder.getX() + floatingBox.getX());
+		floatingBox.setY(levelHolder.getY() + floatingBox.getY());
+		
+		ground = new Circle(gc.getWidth()/2, gc.getHeight()/2, (level.getWidth() - (140 * scale))/2);
 		
 	}
 
@@ -40,9 +56,14 @@ public class GameWindow extends BasicGameState {
 		g.setColor(Color.white);
 		g.fill(background);
 		g.setColor(Color.orange);
-		g.fill(rectangle);
+		g.fill(attachedBox);
+		g.fill(floatingBox);
 		g.setColor(Color.red);
-		g.draw(rectangle);
+		g.draw(attachedBox);
+		g.draw(floatingBox);
+		g.draw(levelHolder);
+		g.setColor(Color.pink);
+		g.draw(ground);
 		
 		level.drawCentered(gc.getWidth()/2, gc.getHeight()/2);
 		
@@ -52,7 +73,10 @@ public class GameWindow extends BasicGameState {
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException 
 	{
 		level.rotate((float) (.09 * delta));
-		
+		levelHolder = levelHolder.transform(Transform.createRotateTransform((float) Math.toRadians(.09 * delta), gc.getWidth()/2, gc.getHeight()/2));
+		attachedBox = attachedBox.transform(Transform.createRotateTransform((float) Math.toRadians(.09 * delta), gc.getWidth()/2, gc.getHeight()/2));
+		floatingBox = floatingBox.transform(Transform.createRotateTransform((float) Math.toRadians(.09 * delta), gc.getWidth()/2, gc.getHeight()/2));
+		ground = ground.transform(Transform.createRotateTransform((float) Math.toRadians(.09 * delta), gc.getWidth()/2, gc.getHeight()/2));
 	}
 
 	@Override
